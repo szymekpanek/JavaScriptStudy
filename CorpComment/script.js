8624ff512fffe9b6207b3e59c66d554bdc755a94
szymekpanek <szympanek2002@gmail.com>
2024-12-18 19:44:40 +0100

    word Analytics project complete

--- CorpComment/script.js
+++ CorpComment/script.js
@@ -1,11 +1,35 @@
 // -- GLOBAL -- //
 const MAX_CHARS = 150;
+const URL = 'https://bytegrad.com/course-assets/js/1/api';
+
 const textAreaEL = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedBackListEL = document.querySelector('.feedbacks');
 const submitBtnEL = document.querySelector('.submit-btn');
 const spinnerEl = document.querySelector('.spinner');
+const HashTagListEL = document.querySelector('.hashtags');
+
+const renderFeedbackItem = feedbackItem => {
+    const feedbackItemHTML = `
+    <li class="feedback">
+    <button class="upvote">
+        <i class="fa-solid fa-caret-up upvote__icon"></i>
+        <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+    </button>
+    <section class="feedback__badge">
+        <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+    </section>
+    <div class="feedback__content">
+        <p class="feedback__company">${feedbackItem.company}</p>
+        <p class="feedback__text">${feedbackItem.text}</p>
+    </div>
+        <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' :  '${feedbackItem.daysAgo}d'}</p>
+    </li>`
+
+    feedBackListEL.insertAdjacentHTML('beforeend', feedbackItemHTML);
+}
+
 // -- COUNTER COMPONENT -- //
 
 const inputHandler = () => {
@@ -47,24 +71,28 @@ const submitHandler = event => {
     const badgeLetter = company.substring(0 , 1).toUpperCase();
     const upvoteCount = 0;
     const daysAgo = 0;
-    
-    const feedbackItemHTML = `
-    <li class="feedback">
-    <button class="upvote">
-        <i class="fa-solid fa-caret-up upvote__icon"></i>
-        <span class="upvote__count">${upvoteCount}</span>
-    </button>
-    <section class="feedback__badge">
-        <p class="feedback__letter">${badgeLetter}</p>
-    </section>
-    <div class="feedback__content">
-        <p class="feedback__company">${company}</p>
-        <p class="feedback__text">${text}</p>
-    </div>
-        <p class="feedback__date">${daysAgo === 0 ? 'NEW' :  '${daysAgo}d'}</p>
-    </li>`
 
-    feedBackListEL.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    const feedbackItem = {
+        upvoteCount,
+        company,
+        daysAgo,
+        badgeLetter,
+        text
+    };
+    renderFeedbackItem(feedbackItem);
+
+    fetch(`${URL}/feedbacks`, {
+        method: 'POST', 
+        body: JSON.stringify(feedbackItem),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if(!response.ok){return console.log('sth went wrong')}
+        console.log('OK');
+        }).catch(e => console.log(e));
+
 
     textAreaEL.value = '';
     submitBtnEL.blur();
@@ -75,32 +103,55 @@ formEl.addEventListener('submit', submitHandler);
 
 // -- FEEDBACK LIST COMPONENT -- //
 
-fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+const clickHandler = () => {
+    const clickedEl = event.target;
+
+    const upvoteIntention = clickedEl.className.includes('upvote');
+    
+    if(upvoteIntention){
+        const upvoteBtnEl = clickedEl.closest('.upvote');
+        upvoteBtnEl.disabled = true;
+
+        const upvoteCountEl = upvoteBtnEl.querySelector('.upvote__count'); //important
+        let upvoteCount = +upvoteCountEl.textContent;
+
+        upvoteCountEl.textContent = ++upvoteCount;
+
+    }else clickedEl.closest('.feedback').classList.toggle('feedback--expand');
+};
+
+feedBackListEL.addEventListener('click', clickHandler);
+
+fetch(`${URL}/feedbacks`)
     .then(response => response.json())
     .then(data => {
         spinnerEl.remove();
-    
-        data.feedbacks.forEach(element => {
-            const feedbackItemHTML = `
-            <li class="feedback">
-            <button class="upvote">
-                <i class="fa-solid fa-caret-up upvote__icon"></i>
-                <span class="upvote__count">${element.upvoteCount}</span>
-            </button>
-            <section class="feedback__badge">
-                <p class="feedback__letter">${element.badgeLetter}</p>
-            </section>
-            <div class="feedback__content">
-                <p class="feedback__company">${element.company}</p>
-                <p class="feedback__text">${element.text}</p>
-            </div>
-                <p class="feedback__date">${element.daysAgo === 0 ? 'NEW' :  `${element.daysAgo}d`}</p>
-            </li>`;
-        
-            feedBackListEL.insertAdjacentHTML('beforeend', feedbackItemHTML);
-        });
+        data.feedbacks.forEach(feedbackItem => renderFeedbackItem(feedbackItem));
     })
     .catch(error => {
         feedBackListEL.textContent = `Failed to fetch. Error message: ${error.message}`;
     });
     
+
+// -- HASHTAG LIST COMPONENT -- 
+const HashTagClickHandler = event => {
+    const clickedEl = event.target;
+
+    if(clickedEl.className === 'hashtags') return; 
+
+    const companyNameFromHashtag = clickedEl.textContent.substring(1).toLowerCase().trim();
+
+    feedBackListEL.childNodes.forEach(childNode => {
+        if(childNode.nodeType === 3) return; 
+
+        const companyNameFromFeedbackItem = childNode.querySelector('.feedback__company').textContent.toLowerCase().trim();
+
+        if(companyNameFromHashtag !== companyNameFromFeedbackItem){
+            childNode.remove();
+        }
+    });
+
+    
+};
+
+HashTagListEL.addEventListener('click', HashTagClickHandler);
